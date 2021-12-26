80524912f43c702df04f1c36c415c17389ab1b3c
deepakphpweb <dipakrana2760@gmail.com>
2021-12-26 08:38:38 +0530

    [Added : Deepak ]- Welcome to JavaIO file system

--- src/main/java/com/blz/javaio/EmployeePayrollService.java
+++ src/main/java/com/blz/javaio/EmployeePayrollService.java
@@ -1,2 +1,7 @@
-package com.blz.javaio;public class EmployeePayrollService {
+package com.blz.javaio;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to javaIO File");
+    }
 }
